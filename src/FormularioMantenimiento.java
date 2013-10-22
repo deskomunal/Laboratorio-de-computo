251689272b469962893cd0aeccd8b1e8e3149956
mrtrash <mrtrash@mrtrash-desktop.(none)>
2013-10-21 23:15:07 -0400

    modificado de la clase FormularioMantenimiento.java

--- src/FormularioMantenimiento.java
+++ src/FormularioMantenimiento.java
@@ -3,7 +3,9 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.crypto.Cipher;
 import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
@@ -30,37 +32,65 @@ class FormularioMantenimiento extends JPanel implements ActionListener{
 	private JComboBox<String> cIdMaq, cPerMan;
 	// radioButton tipo de mantenimiento
 	private JRadioButton rTipo1, rTipo2;
+	private ButtonGroup tipo;
 	// area de texto para la descripcion del mantenimieto
 	private JTextArea tDesMan;
 	private JButton bAceptar, bCancelar, bLimpiar;
+	
 	public FormularioMantenimiento() {
-		
+		setLayout(null);
+		setBounds(50,50,750,450);
+		ajustarComponente();
+		agregarOyenter();
+		agregarComponentes();
+		setVisible(true);
+	}
+	
+	private void ajustarComponente(){
 		lIdMaq = new JLabel("ID Maquina:");
+		lIdMaq.setBounds(10, 20, 200, 20);
 		lTipMan = new JLabel("Tipo de Mantenimiento:");
+		lTipMan.setBounds(10, 70, 200, 20);
 		lDesMan = new JLabel("Descripcion Mantenimiento:");
+		lDesMan.setBounds(10, 120, 200, 20);
 		lPerMan = new JLabel("Personal de Mantenimiento:");
+		lPerMan.setBounds(10, 270, 200, 20);
 		lFecMan = new JLabel("Fecha:");
+		lFecMan.setBounds(10, 320, 200, 20);
 		cIdMaq = new JComboBox<String>();
+		cIdMaq.setBounds(210, 20, 200, 30);
 		cPerMan = new JComboBox<String>();
-		rTipo1 = new JRadioButton("Reparacion");
+		cPerMan.setBounds(210, 270, 200, 30);
+		rTipo1 = new JRadioButton("Reparacion",true);
+		rTipo1.setBounds(210, 70, 120, 20);
 		rTipo2 = new JRadioButton("Cambio de accesorio");
-		JPanel radioB = new JPanel();
-		radioB.setLayout(new GridLayout(5,2));
-		radioB.add(rTipo1);
-		radioB.add(rTipo2);
+		rTipo2.setBounds(340, 70, 180, 20);
+		tipo = new ButtonGroup();
+		tipo.add(rTipo1);
+		tipo.add(rTipo2);
+		
 		tDesMan = new JTextArea();
+		tDesMan.setBounds(210, 120, 200, 150);
 		bAceptar = new JButton("Aceptar");
-		bAceptar.addActionListener(this);
+		bAceptar.setBounds(20, 350, 150, 30);
 		bCancelar = new JButton("Cancerlar");
+		bCancelar.setBounds(180, 350, 150, 30);
 		bLimpiar = new JButton("Limpiar");
-		
-		setLayout(new GridLayout(6,2));
-		setBounds(0, 0, 400, 600);
-		
+		bLimpiar.setBounds(340, 350, 150, 30);
+	}
+	//metodo de agregar listener a los botones
+	private void agregarOyenter(){
+		bAceptar.addActionListener(this);
+		bCancelar.addActionListener(this);
+		bLimpiar.addActionListener(this);
+	}
+	//metodo para agregar componentes al jPanel
+	private void agregarComponentes(){
 		add(lIdMaq);
 		add(cIdMaq);
 		add(lTipMan);
-		add(radioB);
+		add(rTipo1);
+		add(rTipo2);
 		add(lDesMan);
 		add(tDesMan);
 		add(lPerMan);
@@ -69,15 +99,28 @@ class FormularioMantenimiento extends JPanel implements ActionListener{
 		add(bAceptar);
 		add(bCancelar);
 		add(bLimpiar);
-		setVisible(true);
-		
-		
 	}
+	//acciones de los botones
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		System.out.print("algo");
-		if(){
-			System.out.println("descripcion "+desMan.toString());
-		}		
+		Object objeto = e.getSource();
+		if(objeto instanceof JButton){
+			//evento al presionar el boton aceptar
+			if(objeto == bAceptar){
+				//cIdMaq.get
+				desMan = tDesMan.getText();
+				
+			}
+			//evento al presionar el boton cancela
+			if(objeto == bCancelar){
+				System.out.println("se cancelo");
+			}
+
+			//evento al presionar el boton limpiar
+			if(objeto == bLimpiar){
+				System.out.println("se limpio");
+			}
+		}
+		
 	}
 }
